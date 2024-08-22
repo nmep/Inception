267073d22b28e8d85ec2e5776697ede2dfb6b121
garfi <garfi@Inception.myguest.virtualbox.org>
2024-08-22 23:10:09 +0200

    Qwer

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-service mysql start;
+service mariadb start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+(mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" && echo "create dabase success") || echo "create db failed"
 
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+(mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" && echo "create user success") || echo "create user failed"
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+(mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" &&  echo sql user got all privilege sucess) || echo sql user grant privilege failed
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Set root password
+(mysql -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" && echo "alter user success") || echo "sql root user password changed failed"; 
 
-mysql -e "FLUSH PRIVILEGES;"
+# Flush privileges
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
+# (mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" && echo "alter user success") || echo sql root user pssword changed failed
+
+if mysqladmin -u root -p$SQL_ROOT_PASSWORD ping > /dev/null 2>&1; then
+    (mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown && echo shutdown success) || echo shutdown failed
+fi
 
 exec mysqld_safe
